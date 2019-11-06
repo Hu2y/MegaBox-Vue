12a9d48437e293082a4b8cb534376f3114ae6e2d
Hu2y <sanghun_@daum.net>
2019-11-06 19:59:16 +0900

    *banner_menu component html - markup

--- src/components/content-components/BannerMenu.vue
+++ src/components/content-components/BannerMenu.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>banner menu</div>
+  <div class="banner_menu">
+    <div class="container">
+      <div class="row">
+        <div class="bm_left">
+          <ul>
+            <li class="total"><a href="">전체메뉴</a></li>
+            <li class="line"><a href="">필름 소사이어티</a></li>
+            <li><a href="">클래식 소사이어티</a></li>
+          </ul>
+        </div>
+        <div class="bm_right">
+          <ul>
+            <li class="line"><a href="">고객센터</a></li>
+            <li class="line"><a href="">멤버십</a></li>
+            <li><a href="">VIP</a></li>
+          </ul>
+          <ul>
+            <li class="white"><a href="">상영시간표</a></li>
+            <li class="purple"><a href="">빠른예매<img src="assets/img/test.png" alt=""></a></li><!-- 이미지 추가햇음 -->
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
