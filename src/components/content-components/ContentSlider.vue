82abbe2b913c58129fe54bd0b6e647bd76f9701e
Hu2y <sanghun_@daum.net>
2019-11-11 01:20:48 +0900

    *components  - slider base

--- src/components/content-components/ContentSlider.vue
+++ src/components/content-components/ContentSlider.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>slider</div>
+  <div class="slider">
+    <swiper :options="swiperOption" class="sliderCont">
+    <swiper-slide>
+      <span class="textSlider">Slide 1</span>
+    </swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    <swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide>
+    <swiper-slide>Slide 9</swiper-slide>
+    <swiper-slide>Slide 10</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+    <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div>
+  </swiper>
+  </div>
+  
 </template>
 
 <script>
-export default {
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 
-}
+export default {
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      }
+    };
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
