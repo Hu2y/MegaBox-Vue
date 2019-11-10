1c160e3a19cb68a18ac219bc4df52724a7241b6c
Hu2y <sanghun_@daum.net>
2019-11-11 01:37:54 +0900

    *swiper component  - markup(90%) / css

--- src/components/content-components/ContentSlider.vue
+++ src/components/content-components/ContentSlider.vue
@@ -1,24 +1,62 @@
+<!--
 <template>
   <div class="slider">
-    <swiper :options="swiperOption" class="sliderCont">
-    <swiper-slide>
-      <span class="textSlider">Slide 1</span>
-    </swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
-    <swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide>
-    <swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide>
-    <swiper-slide>Slide 9</swiper-slide>
-    <swiper-slide>Slide 10</swiper-slide>
-    <div class="swiper-pagination" slot="pagination"></div>
-    <div class="swiper-button-prev" slot="button-prev"></div>
-    <div class="swiper-button-next" slot="button-next"></div>
-  </swiper>
+    <div class="container">
+      <div class="row">
+        <swiper :options="swiperOption" class="sliderCont">
+          <swiper-slide class="swiper-slide ss1">
+            <h2>캡틴 마블<em>Captain Marvel</em></h2>
+            <p>새로운 히어로, 어벤져스의 희망</p>
+          </swiper-slide>
+          <swiper-slide class="swiper-slide ss2">
+            <h2>Avengers<em>End Game</em></h2>
+            <p>역대 최단 기간 1000만 관객 돌파 기록</p>
+          </swiper-slide>
+          <swiper-slide class="swiper-slide ss3">
+            <h2>Game Of Thrones <em>Season 3</em></h2>
+            <p>The Complete Third Season</p>
+          </swiper-slide>
+          <swiper-slide class="swiper-slide ss4">
+            <h2>Game Of Thrones <em>Season 3</em></h2>
+            <p>The Complete Third Season</p>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+    </div>
   </div>
-  
+</template>
+-->
+<template>
+  <div class="slider">
+    <div class="container">
+      <div class="row">
+        <swiper :options="swiperOption" class="sliderCont">
+          <swiper-slide class="swiper-slide ss1">
+            <h2>캡틴 마블<em>Captain Marvel</em></h2>
+            <p>새로운 히어로, 어벤져스의 희망</p>
+          </swiper-slide>
+          <swiper-slide class="swiper-slide ss2">
+            <h2>Avengers<em>End Game</em></h2>
+            <p>역대 최단 기간 1000만 관객 돌파 기록</p>
+          </swiper-slide>
+          <swiper-slide class="swiper-slide ss3">
+            <h2>Game Of Thrones <em>Season 3</em></h2>
+            <p>The Complete Third Season</p>
+          </swiper-slide>
+          <swiper-slide class="swiper-slide ss4">
+            <h2>Game Of Thrones <em>Season 3</em></h2>
+            <p>The Complete Third Season</p>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
