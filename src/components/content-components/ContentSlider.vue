651f2f2193c64463658b29bfc30dace3d33fa926
Hu2y <sanghun_@daum.net>
2019-11-11 02:07:48 +0900

    *slider component  - swiper.css

--- src/components/content-components/ContentSlider.vue
+++ src/components/content-components/ContentSlider.vue
@@ -31,24 +31,40 @@
 -->
 <template>
   <div class="slider">
-    <div class="container">
-      <div class="row">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
         <swiper :options="swiperOption" class="sliderCont">
-          <swiper-slide class="swiper-slide ss1">
-            <h2>캡틴 마블<em>Captain Marvel</em></h2>
-            <p>새로운 히어로, 어벤져스의 희망</p>
+          <swiper-slide class="swiper-slider ss1">
+            <div class="container">
+              <div class="row">
+                <h2>캡틴 마블 <em>Captain Marvel</em></h2>
+                <p>새로운 히어로, 어벤져스의 희망</p>
+              </div>
+            </div>
           </swiper-slide>
-          <swiper-slide class="swiper-slide ss2">
-            <h2>Avengers<em>End Game</em></h2>
-            <p>역대 최단 기간 1000만 관객 돌파 기록</p>
+          <swiper-slide class="swiper-slider ss2">
+            <div class="container">
+              <div class="row">
+                <h2>Avengers<em>End Game</em></h2>
+                <p>역대 최단 기간 1000만 관객 돌파 기록</p>
+              </div>
+            </div>
           </swiper-slide>
-          <swiper-slide class="swiper-slide ss3">
-            <h2>Game Of Thrones <em>Season 3</em></h2>
-            <p>The Complete Third Season</p>
+          <swiper-slide class="swiper-slider ss3">
+            <div class="container">
+              <div class="row">
+                <h2>Avengers<em>End Game</em></h2>
+                <p>역대 최단 기간 1000만 관객 돌파 기록</p>
+              </div>
+            </div>
           </swiper-slide>
-          <swiper-slide class="swiper-slide ss4">
-            <h2>Game Of Thrones <em>Season 3</em></h2>
-            <p>The Complete Third Season</p>
+          <swiper-slide class="swiper-slider ss4">
+            <div class="container">
+              <div class="row">
+                <h2>Game Of Thrones <em>Season 3</em></h2>
+                <p>The Complete Third Season</p>
+              </div>
+            </div>
           </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
           <div class="swiper-button-prev" slot="button-prev"></div>
@@ -60,7 +76,8 @@
 </template>
 
 <script>
-import "swiper/dist/css/swiper.css";
+// import "swiper/dist/css/swiper.css";
+import "../../assets/css/swiper.css"
 import { swiper, swiperSlide } from "vue-awesome-swiper";
 
 export default {
